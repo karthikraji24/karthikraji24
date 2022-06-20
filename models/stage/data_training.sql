1b58a9260ef705f8c1ad1d4a53bf96ace10d7ef1
karthik.raji@techigai.io <karthik.raji@techigai.io>
2022-06-20 13:42:38 +0000

    commit

--- models/stage/data_training.sql
+++ models/stage/data_training.sql
@@ -1,6 +1,7 @@
 with data_training as (
 
-    select *,duration_days from TECHIGAI.DATA_TRAINING
+    select couseid as course_id,course_name,trainer,datediff(day,starttime,endtime) as duration_days,starttime as startdate, endtime as enddate,
+    grade,domain from TECHIGAI.DATA_TRAINING
 )
 
 select * from data_training
