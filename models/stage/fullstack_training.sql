aca9635d8564a6fe422d9e93c24583f205697ea1
karthik.raji@techigai.io <karthik.raji@techigai.io>
2022-06-20 10:55:34 +0000

    column corrected

---
+++ models/stage/fullstack_training.sql
@@ -0,0 +1,9 @@
+with fullstack_training as (
+
+
+  select couseid as course_ID,course_name,datediff(day,starttime,endtime) as duration_days,trainer,
+  starttime,endtime,grade,domain from techigai.training
+
+)
+
+select * from fullstack_training
